59da24f7396f16618831b8f48bd566baf5c08969
Adjoa Wadee <adjoa.wadee@gmail.com>
2023-02-16 15:32:50 +0100

    changes to router.ts and Services page set up

--- app/components/Diensten.vue
+++ app/components/Diensten.vue
@@ -1,3 +1,50 @@
 <template>
-    <div class="w-screen min-h-[100vh] max-w-full bg-cover overflow-hidden" id="diensten">Service</div>
+    <section class="relative flex w-screen min-h-[100vh] max-w-full bg-cover overflow-hidden" id="diensten">
+        <div class="mx-auto">
+            <div class="w-screen">
+              <ul class="lg:grid lg:grid-cols-3 lg:gap-1 ">
+               <li class="flex lg:py-8 items-center justify-center">
+                    <div class="flex py-4 px-6 border-b-4 border-amber-500">
+                        <div class="px-3 pt-4">      
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0" />
+                        </svg>
+                        </div>
+                        <div>
+                            <span class="text-4xl sm:text-5xl md:text-5xl xl:text-6xl font-bold mb-2" data-value="50">00+</span>
+                            <p class="text-lg sm:text-xl lg:text-2xl xl:text-3xl">tevreden klanten</p>
+                        </div>
+                    </div>
+               </li>
+               <li class="flex lg:py-8 items-center justify-center">
+                <div class="flex py-4 px-6 border-b-4 border-amber-500">
+                    <div class="px-3 pt-4">      
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
+                        </svg>
+
+                    </div>
+                    <div>
+                        <span class="text-4xl sm:text-5xl md:text-5xl xl:text-6xl font-bold mb-2">200+</span>
+                        <p class="text-lg sm:text-xl lg:text-2xl xl:text-3xl">antal klussen</p>
+                    </div>
+                </div>
+               </li>
+               <li class="flex lg:py-8 items-center justify-center">
+                <div class="flex py-4 px-6 border-b-4 border-amber-500">
+                    <div class="px-3 pt-4">      
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <span class="text-4xl sm:text-5xl md:text-5xl xl:text-6xl font-bold mb-2">2021</span>
+                        <p class="text-lg sm:text-xl lg:text-2xl xl:text-3xl">begonnen wij</p>
+                    </div>
+                </div>
+               </li>
+            </ul>
+            </div>
+        </div>
+    </section>
 </template>
